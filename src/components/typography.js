a901543aea8f1ba19794ccab13f4e93b6bb12a9a
xavier <xavier.arul@capestart.com>
2019-04-15 18:53:56 +0530

    Typography resetstyle

--- src/components/typography.js
+++ src/components/typography.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import '../styles/global.scss';
 
 export class Dashboard extends Component {
   render() {
     return (
       <div>
         <h1>Typography</h1>
+        <div className="cover"></div>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
+import './styles/resetStyle.scss';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import Typography from './components/Typography';
 
 ReactDOM.render(
     <BrowserRouter>
         <React.Fragment>
             <Route path="/dashboard" component={Dashboard} />
-            
+            <Route path="/typography" component={Typography} />
         </React.Fragment>
     </BrowserRouter>
 
