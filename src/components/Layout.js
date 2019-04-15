2bc06737a56e14e2e7c6948f2e282e5a99c6950a
bhagavathavigo <38946625+bhagavathavigo@users.noreply.github.com>
2019-04-15 20:53:17 +0530

    UI conflict resolved

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -15,7 +15,7 @@ export class Layout extends Component {
                         </div>
                         <div className="nav_right">
                             <div className="header_search">
-                                {/* <img src={require('./images/serach.svg')} /> */}
+                                {/* <img src={require('../images/serach.svg')} /> */}
                                 <input class="input-field" type="text" placeholder="Username" name="usrnm" />
                             </div>
                             <div className="header_notification"></div>
