b3e20994b0da24dc86774e78384dd2e0071acc7f
bhagavathavigo <38946625+bhagavathavigo@users.noreply.github.com>
2019-04-15 19:14:14 +0530

    UI menu layout

---
+++ src/components/Layout.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import '../styles/layout.scss';
+
+export class Layout extends Component {
+  render() {
+    return (
+        <React.Fragment>
+            <h1>Menu</h1>
+            <hr/>
+            <div className="cs_navbar">
+                <div class="container">
+                    <div className="navcontainer">
+                        <div className="nav_left">
+                            <h1>LOGO</h1>
+                        </div>
+                        <div className="nav_right">
+                            <div className="header_search">
+                                <img src={require('./images/serach.svg')} />
+                                <input class="input-field" type="text" placeholder="Username" name="usrnm" />
+                            </div>
+                            <div className="header_notification"></div>
+                            <div className="header_profile"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+    )
+  }
+}
+
+export default Layout
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import Layout from './components/Layout';
 
 ReactDOM.render(
     <BrowserRouter>
         <React.Fragment>
             <Route path="/dashboard" component={Dashboard} />
-            
+            <Route path="/layout" component={Layout} />
         </React.Fragment>
     </BrowserRouter>
 
