b48f768ada0e16c3c95d4b68b8596a9371998bac
Brittoui <30798891+Brittoui@users.noreply.github.com>
2019-04-16 15:22:05 +0530

    sass creation

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import FormElements from './components/FormElements';
 
 ReactDOM.render(
     <BrowserRouter>
         <React.Fragment>
             <Route path="/dashboard" component={Dashboard} />
+            <Route path="/FormElements" component={FormElements} />
             
         </React.Fragment>
     </BrowserRouter>
