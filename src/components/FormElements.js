d104b07ece57abab7f24e461079e4d5007bcc1e1
Brittoui <30798891+Brittoui@users.noreply.github.com>
2019-04-17 18:11:35 +0530

    sass creation

--- src/components/FormElements.js
+++ src/components/FormElements.js
@@ -51,6 +51,14 @@ export class FormElements extends Component {
     <label for="i2">Item 1</label>
   </div>
 </div>
+<div class="chip">
+  <div class="chip-head">C</div>
+  <div class="chip-content">Chip Content</div>
+  <div class="chip-close">
+    <svg class="chip-svg" focusable="false" viewBox="0 0 24 24" aria-hidden="true"><path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z"></path></svg>
+
+  </div>
+</div>
   </form>
 <form>
 <ul class="tg-list">
@@ -75,6 +83,7 @@ export class FormElements extends Component {
     <label class="tgl-btn" data-tg-off="Nope" data-tg-on="Yeah!" for="cb5"></label>
   </li>
 </ul>
+
 </form>
 </div>
     )
