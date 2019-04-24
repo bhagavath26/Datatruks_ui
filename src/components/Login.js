f2cf927305bbe1b69d3c2e377bd34b5849291d3a
bhagavathavigo <38946625+bhagavathavigo@users.noreply.github.com>
2019-04-24 18:58:54 +0530

    UI login page css

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,17 +2,42 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import '../styles/login.scss';
+import '../styles/formElement.scss';
 
 export class Login extends Component {
   render() {
     return (
-      <React.Fragment>  
+      <React.Fragment>
         <Header />
         <div className="cs_logincontainer">
-          
+          <div className="login-box">
+            <div className="login-left">
+              <div className="logo_div">
+                <h3>LOGO</h3>
+              </div>
+              <h3>Super Easy Data Annotations</h3>
+              <h6>Invite your team and generate high quality labeled data in minutes</h6>
+              <small>By signing up on Dataturks, you agree with our privacy policy and terms.</small>
+            </div>
+            <div className="login-right">
+            <h5>LOGIN</h5>
+              <form class="cs_form_md">
+                  <div class="cs_form_group">
+                    <input id="username" class="form-control" type="text" required />
+                    <label for="username">Username</label>
+                  </div>
+                  <div class="cs_form_group">
+                    <input id="password" class="form-control" type="password" required />
+                    <label for="password">Password</label>
+                  </div>
+                  <button className="cs_btnbase cs_btnprimary">LOGIN</button> 
+                </form>
+                <a href="">Forgot your password?</a>
+            </div>
+          </div>
         </div>
         <Footer />
-      </React.Fragment>  
+      </React.Fragment>
     )
   }
 }
